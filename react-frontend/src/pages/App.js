c8617c543a2f0b67a5fede2d513f53517806d232
Kris <magikabdul@users.noreply.github.com>
2022-03-03 00:11:42 +0100

    [WOR-22] add styled components and theme (#4)
    * [WOR-22] light and dark theme configuration
    
    * [WOR-22] Montserrat font added
    
    * [WOR-22] styled-components, GlobalStyles

--- react-frontend/src/pages/App.js
+++ react-frontend/src/pages/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import GlobalStyles from 'theme/GlobalStyles';
+import darkTheme from 'theme/darkTheme';
+import lightTheme from 'theme/lightTheme';
+import { ThemeProvider } from 'styled-components';
 
-const App = () => (
-  <div>
-    <p>hello</p>
-  </div>
-);
+const App = () => {
+  const [darkMode, setDarkMode] = useState(true);
+
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <GlobalStyles theme={darkMode ? darkTheme : lightTheme} />
+      <p>hello</p>
+      <button type='button' onClick={() => setDarkMode(!darkMode)}>
+        toggle
+      </button>
+    </ThemeProvider>
+  );
+};
 
 export default App;
