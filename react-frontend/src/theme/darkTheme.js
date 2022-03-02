16472e1d71f11af5385133af66a1df70d90c9cbc
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-03-03 00:07:55 +0100

    [WOR-22] light and dark theme configuration

---
+++ react-frontend/src/theme/darkTheme.js
@@ -0,0 +1,20 @@
+const darkTheme = {
+  color: {
+    primary: '#3F80EA',
+    secondary: '#7F838E',
+    success: '#4BBF73',
+    info: '#CC8B37',
+    warning: '#CC8B37',
+    danger: '#D9534F',
+    text: {
+      primary: '#FFFFFF',
+      secondary: '#898e99',
+    },
+    background: {
+      primary: '#202634',
+      secondary: '#293042',
+    },
+  },
+};
+
+export default darkTheme;
---
+++ react-frontend/src/theme/lightTheme.js
@@ -0,0 +1,20 @@
+const lightTheme = {
+  color: {
+    primary: '#3F80EA',
+    secondary: '#7F838E',
+    success: '#4BBF73',
+    info: '#CC8B37',
+    warning: '#CC8B37',
+    danger: '#D9534F',
+    text: {
+      primary: '#495057',
+      secondary: '#6C757D',
+    },
+    background: {
+      primary: '#F7F9FC',
+      secondary: '#FFFFFF',
+    },
+  },
+};
+
+export default lightTheme;
