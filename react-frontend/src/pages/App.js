c2a01bd51a2dc69e5552d28488dee16da462c3a6
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-03-03 12:28:23 +0100

    [WOR-16] added RegisterPage.js

--- react-frontend/src/pages/App.js
+++ react-frontend/src/pages/App.js
@@ -7,6 +7,7 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from 'pages/HomePage/HomePage';
 import NotFoundPage from 'pages/NotFoundPage';
 import LoginPage from 'pages/LoginPage';
+import RegisterPage from 'pages/RegisterPage';
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(true);
@@ -21,6 +22,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </ThemeProvider>
---
+++ react-frontend/src/pages/RegisterPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RegisterPage = () => {
+  return (
+    <>
+      <h1>register</h1>
+      <button type='button'>register</button>
+      <button type='button'>login</button>
+    </>
+  );
+};
+
+export default RegisterPage;
