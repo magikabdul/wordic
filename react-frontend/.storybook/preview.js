df3970f5f3056fa9aeb1bea9da4301d4c60364c5
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-03-03 17:38:20 +0100

    [WOR-21] storybook configuration and example component and its story

--- react-frontend/.storybook/preview.js
+++ react-frontend/.storybook/preview.js
@@ -1,3 +1,9 @@
+import { addDecorator } from '@storybook/react';
+import { withThemes } from '@react-theming/storybook-addon/dist/preview';
+import { ThemeProvider } from 'styled-components';
+import theme from '../src/theme/darkTheme';
+import GlobalStyles from '../src/theme/GlobalStyles';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +13,6 @@ export const parameters = {
     },
   },
 }
+
+addDecorator(withThemes(ThemeProvider, [theme]));
+addDecorator(s => <><GlobalStyles theme={theme}/>{s()}</>);
